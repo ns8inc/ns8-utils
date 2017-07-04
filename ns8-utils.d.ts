69273c821b41d0a1426dfa49cf6b7c1af3edd928
Adam Rogas <adam@rogassi.com>
2017-07-04 00:10:58 -0700

    format

--- ns8-utils.d.ts
+++ ns8-utils.d.ts
@@ -1,36 +1,36 @@
 
 declare module 'ns8-utils' {
-    export function superRandom(bytes?:number):string;
-    export function isArray(obj:any):boolean;
-    export function prefixAttributes(obj:Object, prefix:string):Object;
-    export function isNumeric(n:any):boolean;
-    export function currencyToNumber(n:any):number;
-    export function toBytes(num:number):Uint8Array;
-    export function uuid(len?:number, radix?:number):string;
-    export function base62encode(i:any):string;
-    export function isUTF8(charset:any):boolean;
-    export function padLeft(value:any, padChar:string, padCount:number):string;
-    export function urlDecode(str:any, charset:any):any;
-    export function clone(o:Object):Object;
-    export function extend(o1:Object, o2:Object):Object;
-    export function copyProperty(property:string, o1:Object, o2:Object, allowNull?:boolean, allowEmpty?:boolean):boolean;
-    export function isFunction(functionToCheck:any):boolean;
-    export function replaceAll(original:string, search:string, replacement:string):string;
-    export function htmlify(obj:any):string;
-    export function empty(obj:Object):boolean;
-    export function trimStart(value:string, start:string):string;
-    export function parseCookieValueFromString(headerVal:string, key:string):any;
-    export function trimEnd(val:string, end:string):string;
-    export function repeat(val:string, n:number):string;
-    export function spaces(n:number):string;
-    export function merge(o1:any, o2:any, overwrite:boolean):Object;
-    export function validEmail(email:string):boolean;
-    export function missingParams(req:any, requiredParams:string[]):string[];
-    export function guid():string;
-    export function hash(input:string):number;
-    export function noCache(res:any):void;
-    export function stripProtocol(url:string):string;
-    export function parseBoolean(value, nullOnFailure?:boolean);
+    export function superRandom(bytes?: number): string;
+    export function isArray(obj: any): boolean;
+    export function prefixAttributes(obj: Object, prefix: string): Object;
+    export function isNumeric(n: any): boolean;
+    export function currencyToNumber(n: any): number;
+    export function toBytes(num: number): Uint8Array;
+    export function uuid(len?: number, radix?: number): string;
+    export function base62encode(i: any): string;
+    export function isUTF8(charset: any): boolean;
+    export function padLeft(value: any, padChar: string, padCount: number): string;
+    export function urlDecode(str: any, charset: any): any;
+    export function clone(o: Object): Object;
+    export function extend(o1: Object, o2: Object): Object;
+    export function copyProperty(property: string, o1: Object, o2: Object, allowNull?: boolean, allowEmpty?: boolean): boolean;
+    export function isFunction(functionToCheck: any): boolean;
+    export function replaceAll(original: string, search: string, replacement: string): string;
+    export function htmlify(obj: any): string;
+    export function empty(obj: Object): boolean;
+    export function trimStart(value: string, start: string): string;
+    export function parseCookieValueFromString(headerVal: string, key: string): any;
+    export function trimEnd(val: string, end: string): string;
+    export function repeat(val: string, n: number): string;
+    export function spaces(n: number): string;
+    export function merge(o1: any, o2: any, overwrite: boolean): Object;
+    export function validEmail(email: string): boolean;
+    export function missingParams(req: any, requiredParams: string[]): string[];
+    export function guid(): string;
+    export function hash(input: string): number;
+    export function noCache(res: any): void;
+    export function stripProtocol(url: string): string;
+    export function parseBoolean(value, nullOnFailure?: boolean);
     export function renameAttribute(obj: Object, name: string, replacement: string): Object;
     export function getUrlText(url, callback);
 
@@ -41,18 +41,18 @@ declare module 'ns8-utils' {
     }
 
     export module ip {
-        export function localHost(req:any):boolean;
-        export function isIPV6(address:string):boolean;
-        export function remoteAddress(req:any):string;
-        export function isLoopback(addr:any):boolean;
-        export function hashIPV6(ip:string):number;
-        export function hash(address:string):number;
-        export function toNumber(address:string, reverse?:boolean):number;
-        export function compress(address:string):any;
-        export function decompress(address:string):any;
-        export function toNumber32(address:string):number;
-        export function fromNumber(ipNumber:number):string;
-        export function toNumberIPV6(address:string):number;
+        export function localHost(req: any): boolean;
+        export function isIPV6(address: string): boolean;
+        export function remoteAddress(req: any): string;
+        export function isLoopback(addr: any): boolean;
+        export function hashIPV6(ip: string): number;
+        export function hash(address: string): number;
+        export function toNumber(address: string, reverse?: boolean): number;
+        export function compress(address: string): any;
+        export function decompress(address: string): any;
+        export function toNumber32(address: string): number;
+        export function fromNumber(ipNumber: number): string;
+        export function toNumberIPV6(address: string): number;
     }
 
     export module epoch {
@@ -63,7 +63,7 @@ declare module 'ns8-utils' {
             momentName: string
         }
 
-        export let timezones:{ [id: number]: Timezone };
+        export let timezones: { [id: number]: Timezone };
 
         export enum DateIntervals {
             second = 0,
@@ -75,56 +75,56 @@ declare module 'ns8-utils' {
             quarter = 6,
             year = 7,
         }
-        export function getTimezoneId(timezone:any):number;
+        export function getTimezoneId(timezone: any): number;
 
-        export function getTimezone(timezone:any);
+        export function getTimezone(timezone: any);
 
-        export function utcOffset(timezoneId:number):number;
+        export function utcOffset(timezoneId: number): number;
 
-        export function currentDatetime(utcOffset?:number):Date;
+        export function currentDatetime(utcOffset?: number): Date;
 
-        export function toUTC(strDate:string, utcOffset?:number):Date;
+        export function toUTC(strDate: string, utcOffset?: number): Date;
 
-        export function currentDate(utcOffset?:number):Date;
+        export function currentDate(utcOffset?: number): Date;
 
-        export function dateToString(date:Date, includeTime?:boolean):string;
+        export function dateToString(date: Date, includeTime?: boolean): string;
 
-        export function startOfDate(date?:Date):Date;
+        export function startOfDate(date?: Date): Date;
 
-        export function addMonths(months:number, date?:Date):Date;
+        export function addMonths(months: number, date?: Date): Date;
 
-        export function addYears(years:number, date?:Date):Date;
+        export function addYears(years: number, date?: Date): Date;
 
-        export function addDays(days:number, date?:Date):Date;
+        export function addDays(days: number, date?: Date): Date;
 
-        export function addHours(hours:number, date?:Date):Date;
+        export function addHours(hours: number, date?: Date): Date;
 
-        export function addMinutes(minutes:number, date?:Date):Date;
+        export function addMinutes(minutes: number, date?: Date): Date;
 
-        export function addSeconds(seconds:number, date?:Date):Date;
+        export function addSeconds(seconds: number, date?: Date): Date;
 
-        export function diff(interval:DateIntervals, date1:Date, date2?:Date):number;
+        export function diff(interval: DateIntervals, date1: Date, date2?: Date): number;
 
-        export function start():Date;
+        export function start(): Date;
 
-        export function millisecond(date?:Date):number;
+        export function millisecond(date?: Date): number;
 
-        export function second(date?:Date):number;
+        export function second(date?: Date): number;
 
-        export function minute(date?:Date):number;
+        export function minute(date?: Date): number;
 
-        export function hour(date?:Date):number;
+        export function hour(date?: Date): number;
 
-        export function day(date?:Date):number;
+        export function day(date?: Date): number;
 
-        export function week(date?:Date):number;
+        export function week(date?: Date): number;
 
-        export function month(date?:Date):number;
+        export function month(date?: Date): number;
 
-        export function quarter(date?:Date):number;
+        export function quarter(date?: Date): number;
 
-        export function currentMonth(utcOffset?:number):Date;
+        export function currentMonth(utcOffset?: number): Date;
 
-        export function monthCode(date?:Date, utcOffset?:number):string;
+        export function monthCode(date?: Date, utcOffset?: number): string;
     }
 }
